09c62a00bb3edef8d69dedadd4a7511074568f65
Ana Paula Dantas <dantaspereiraana@gmail.com>
2025-04-11 20:34:34 -0300

    criação cards e menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, BigCardComponent, HomeComponent, SmallCardComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
