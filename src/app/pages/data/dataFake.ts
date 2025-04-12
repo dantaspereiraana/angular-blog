5a46de2d4fbf847bcd629813927c9c83dfa2a50f
Ana Paula Dantas <dantaspereiraana@gmail.com>
2025-04-12 10:13:52 -0300

    finalizando projeto

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "CINNAMON ROLL: o queridinho da Sanrio",
+        "description": "ola",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgjN46_-WiB5RJBCS176Q3DE7NwJm2KyI0RQ&s"
+    },
+
+    {
+        "id": "2",
+        "title": "HELLO KITTY: estaria acabando o reinado da rainha da Sanrio?",
+        "description": "ola",
+        "photoCover": "https://lamag.com/.image/t_share/MTk3NTU1ODI3ODgyMTQxMzc2/hkitty.jpg"
+    }
+]
